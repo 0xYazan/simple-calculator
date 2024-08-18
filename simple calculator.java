8e8f262e6b8521d811da899c5e39a8cec8fddeee
Yazan <yazanalthukair@gmail.com>
2024-08-19 02:54:55 +0300

    -

--- simple calculator.java
+++ simple calculator.java
@@ -5,59 +5,48 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Variables to store the numbers and the result
-        int num1, num2;
-        int result = 0; // Initialize result to 0
+        System.out.println("Simple calculator for basic arithmetic operations. \n\n --------------------------------------\n");
 
-        // Variable to store the chosen operation
+        Scanner scanner = new Scanner(System.in);
+        float num1, num2, result = 0;
         char operation;
 
-        // Scanner object to read user input
-        Scanner scanner = new Scanner(System.in);
-
-        // Prompt user to enter the first number
         System.out.println("Enter the first number: ");
         num1 = scanner.nextInt();
 
-        // Prompt user to enter the second number
         System.out.println("Enter the second number: ");
         num2 = scanner.nextInt();
 
-        // Prompt user to choose an operation
         System.out.println("Choose an operation: \n(+) Addition\n(-) Subtraction\n(*) Multiplication\n(/) Division ");
-        operation = scanner.next().charAt(0); // Read the first character of the user input
+        operation = scanner.next().charAt(0);
 
-        // Switch statement to perform the chosen operation
         switch (operation) {
-            case '+': // Addition
+            case '+':
                 result = num1 + num2;
                 break;
 
-            case '-': // Subtraction
+            case '-':
                 result = num1 - num2;
                 break;
 
-            case '*': // Multiplication
+            case '*':
                 result = num1 * num2;
                 break;
 
-            case '/': // Division
-                if (num2 != 0) { // Check to prevent division by zero
+            case '/':
+                if (num2 != 0) {
                     result = num1 / num2;
                 } else {
-                    System.out.println("Error: You can't divide by 0"); // Error message for division by zero
-                    return; // Exit the program
+                    System.out.println("Error: You can't divide by 0");
+                    return;
                 }
                 break;
 
-            default: // Handle invalid operation input
+            default:
                 System.out.println("Invalid operation chosen");
-                return; // Exit the program
+                return;
         }
-
-        // Display the result of the calculation
         System.out.println(num1 + " " + operation + " " + num2 + " = " + result);
-
         scanner.close();
     }
 }
